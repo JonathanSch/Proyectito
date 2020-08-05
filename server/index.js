4b9b1c2030b60acbcd81efbe2bc78ebe12cc85db
JonathanSch <jonathansch7@gmail.com>
2020-08-04 19:48:34 -0500

    cors added

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const router = require('../routes')
+const cors = require('cors')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cors())
 
 app.get('/' , (req,res) =>{
     res.status(200).send({message : "Todo bien"})
